f4864a558b04742472342af86fbfdbef12150bca
Denis K <denis911@gmail.com>
2025-10-25 10:48:02 +0200

    ping_mirror updated - can take long messages with spaces or no message at all

--- ping_mirror.py
+++ ping_mirror.py
@@ -4,14 +4,17 @@ from flask import jsonify
 
 app = Flask('Ping')
 
-@app.route('/ping', methods=['GET'])
+@app.route('/ping', methods=['GET', 'POST'])
 def ping():
-    info = request.get_json()
-    print('info received ', info)
-    return jsonify(info)
+    msg = request.args.get('msg', default='PONG') # .get_json() # if I need to receive json requests like an API
+    print('info received: ', msg)
+    return jsonify(message=msg)    
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9696)
     # host='0.0.0.0' is localhost
     # http://127.0.0.1:9696/ping - open in a web browser
+    # http://127.0.0.1:9696/ping?msg=hello - with custom messages
+    # http://127.0.0.1:9696/ping?msg=long%20way%20to%20knowledge - message with spaces
     # or curl http://127.0.0.1:9696/ping in a terminal
